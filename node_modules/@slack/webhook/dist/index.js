6fca6a22c873249f4f12c032b425b18e55bd1b6d
Aleksey Kulikov <alekseys.kulikov@gmail.com>
2020-03-12 00:38:13 +0100

    reset node_modules + gitignore from master

--- node_modules/@slack/webhook/dist/index.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-/// <reference lib="es2017" />
-Object.defineProperty(exports, "__esModule", { value: true });
-var IncomingWebhook_1 = require("./IncomingWebhook");
-exports.IncomingWebhook = IncomingWebhook_1.IncomingWebhook;
-var errors_1 = require("./errors");
-exports.ErrorCode = errors_1.ErrorCode;
-//# sourceMappingURL=index.js.map

8c78042444dd2d2d4a81a9e3870dd3073686a0e1
Aleksey Kulikov <alekseys.kulikov@gmail.com>
2020-03-12 09:51:41 +0100

    improve problem matchers

--- src/utils/problem-matchers.js
+++ src/utils/problem-matchers.js
@@ -8,8 +8,8 @@ const { getAssertionsByUrl } = require('./lhci-helpers')
  * LHCI assertion output is designed to be readable by humans, not machines, and it's hard to parse with regexp
  * This method enables lhci.json problem matcher for a single string, examples:
  *
- * https://likeeper.com/ – error – offscreen-images – `offscreen-images` failure for `maxLength` assertion, expected **<= 0**, but found **1**.
- * https://likeeper.com/ – warning – mainthread-work-breakdown – `mainthread-work-breakdown` warning for `minScore` assertion, expected **>= 0.9**, but found **0.83**
+ * .github/workflows/LHCI-assert-on-budget.yml|12|error|https://treo.sh/ -> `resource-summary` failure for `maxNumericValue` assertion, expected **<= 51200**, but found **85071**.
+ * .github/workflows/LHCI-urls-interpolation.yml|16|warning|https://treo.sh/ -> `resource-summary` failure for `maxNumericValue` assertion, expected **<= 51200**, but found **85071**.
  *
  * @param {string} resultsPath
  */
@@ -19,10 +19,13 @@ exports.runProblemMatchers = function enableProblemMatcher(resultsPath) {
   const assertionsByUrl = getAssertionsByUrl(resultsPath)
   Object.values(assertionsByUrl).forEach(assertions => {
     assertions.forEach(a => {
+      const file = `.github/workflow/${process.env.GITHUB_WORKFLOW || 'unknown'}.yml`
+      const line = 1
+      const severity = a.level === 'error' ? 'error' : 'warning'
       const message =
-        `\`${a.auditId}\` ${a.level === 'error' ? 'failure' : 'warning'} for \`${a.name}\` assertion, ` +
+        `${a.url} -> \`${a.auditId}\` ${a.level === 'error' ? 'failure' : 'warning'} for \`${a.name}\` assertion, ` +
         `expected **${a.operator} ${a.expected}**, but found **${a.actual}**.`
-      console.log(`${a.url}|${a.level}|${a.auditId}|${message}`)
+      console.log(`${file}|${line}|${severity}|${message}`)
     })
   })
   console.log('::remove-matcher owner=lighthouse-ci-action::')

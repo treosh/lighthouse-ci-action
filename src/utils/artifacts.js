e76ec83252f0bf01782afbda59a26cde6bfcac7d
Aleksey Kulikov <alekseys.kulikov@gmail.com>
2021-07-19 15:24:30 +0200

    fix tests

--- src/utils/artifacts.js
+++ src/utils/artifacts.js
@@ -3,7 +3,7 @@ const fs = require('fs').promises
 const { join } = require('path')
 
 /** @param {string} resultsPath */
-exports.uploadArtifacts = async function uploadArtifacts(resultsPath, artifactName='lighthouse-results') {
+exports.uploadArtifacts = async function uploadArtifacts(resultsPath, artifactName = 'lighthouse-results') {
   const artifactClient = artifact.create()
   const fileNames = await fs.readdir(resultsPath)
   const files = fileNames.map((fileName) => join(resultsPath, fileName))
